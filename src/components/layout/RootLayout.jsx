5bbe909a82d1a2134100c48c07faf9fbcc4146fc
jeho017 <leejeho017@gmail.com>
2024-08-26 22:50:18 +0900

    style: 대시보드 스타일 추가

--- src/components/layout/RootLayout.jsx
+++ src/components/layout/RootLayout.jsx
@@ -13,6 +13,11 @@ export default RootLayout;
 
 const RootLayoutMain = styled.main`
   background-color: #f7f6f9;
+  min-height: 100vh;
 `;
 
-const RootLayoutContents = styled.div``;
+const RootLayoutContents = styled.div`
+  max-width: 768px;
+  margin: 0 auto;
+  padding: 20px;
+`;
--- src/components/todo/TodoDashboard.jsx
+++ src/components/todo/TodoDashboard.jsx
@@ -1,3 +1,4 @@
+import { ClipboardCheck, Ellipsis, Monitor, Video } from "lucide-react";
 import React from "react";
 import styled from "styled-components";
 
@@ -5,18 +6,36 @@ const TodoDashboard = () => {
   return (
     <DashboardSection>
       <DashboardHeader>
-        <h1>Todo Dashboard</h1>
+        <h1>Dashboard</h1>
       </DashboardHeader>
 
       <DashboardCardList>
-        <DashboardCard>
-          16 <br /> New Task
+        <DashboardCard flex="2" color="#e7582b">
+          <div>
+            <ClipboardCheck />
+            <Ellipsis />
+          </div>
+          <p>
+            16 <br /> New Task
+          </p>
         </DashboardCard>
-        <DashboardCard>
-          3 <br /> Completed
+        <DashboardCard flex="1" color="#582be7">
+          <div>
+            <Monitor />
+            <Ellipsis />
+          </div>
+          <p>
+            3 <br /> Completed
+          </p>
         </DashboardCard>
-        <DashboardCard>
-          13 <br /> Pending
+        <DashboardCard flex="1" color="#242424">
+          <div>
+            <Video />
+            <Ellipsis />
+          </div>
+          <p>
+            13 <br /> Pending
+          </p>
         </DashboardCard>
       </DashboardCardList>
     </DashboardSection>
@@ -28,12 +47,13 @@ export default TodoDashboard;
 const DashboardSection = styled.section`
   display: flex;
   flex-direction: column;
-  gap: 4px;
+  gap: 1rem;
 `;
 
 const DashboardHeader = styled.div`
   h1 {
-    font-size: 1rem;
+    font-size: 1.5rem;
+    font-weight: bold;
   }
 `;
 
@@ -41,19 +61,26 @@ const DashboardCardList = styled.div`
   display: flex;
   flex-direction: row;
   gap: 8px;
+  width: 100%;
 `;
 
 const DashboardCard = styled.div`
-  background-color: orange;
+  background-color: ${(props) => props.color};
   padding: 1rem;
   border-radius: 1rem;
-  height: 120px;
-  width: 120px;
+  height: calc((640px / 4));
 
   color: white;
   font-weight: bold;
 
   display: flex;
   flex-direction: column;
-  justify-content: end;
+  justify-content: space-between;
+  flex: ${(props) => props.flex};
+
+  div {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
 `;
