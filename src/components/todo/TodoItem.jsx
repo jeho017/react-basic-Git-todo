8c68c318247ac8ae71ddf51ba6b82bcffe823f54
jeho017 <leejeho017@gmail.com>
2024-08-26 23:38:33 +0900

    style: 투두 아이템 스타일 추가

--- src/components/todo/TodoItem.jsx
+++ src/components/todo/TodoItem.jsx
@@ -1,22 +1,69 @@
 import React from "react";
+import styled from "styled-components";
 
 const TodoItem = ({ todo, toggleCompleted, handleDelete }) => {
   return (
-    <li key={todo.id}>
-      <p
-        style={{
-          textDecoration: todo.completed ? "line-through" : "none",
-        }}
-      >
-        {todo.text} -{" "}
-        {todo.completed ? <span>완료됨</span> : <span>미완료</span>}
-      </p>
-      <button onClick={() => toggleCompleted(todo.id)}>
-        {todo.completed ? "취소" : "완료"}
-      </button>
-      <button onClick={() => handleDelete(todo.id)}>삭제</button>
-    </li>
+    <TaskItem key={todo.id}>
+      <TaskItemContent>
+        <p
+          style={{
+            textDecoration: todo.completed ? "line-through" : "none",
+          }}
+        >
+          {todo.text} -{" "}
+          {todo.completed ? <span>완료됨</span> : <span>미완료</span>}
+        </p>
+      </TaskItemContent>
+      <TaskItemActions>
+        <TaskItemActionButton
+          onClick={() => toggleCompleted(todo.id)}
+          color="#582be7"
+        >
+          {todo.completed ? "취소" : "완료"}
+        </TaskItemActionButton>
+        <TaskItemActionButton
+          onClick={() => handleDelete(todo.id)}
+          color="#f05656"
+        >
+          삭제
+        </TaskItemActionButton>
+      </TaskItemActions>
+    </TaskItem>
   );
 };
 
 export default TodoItem;
+
+const TaskItem = styled.li`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 1rem;
+  border-radius: 1rem;
+  background-color: #ffffff;
+`;
+
+const TaskItemContent = styled.div``;
+
+const TaskItemActions = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TaskItemActionButton = styled.button`
+  color: #ffffff; //text 컬러
+  background-color: ${({ color }) => color};
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
+
+  cursor: pointer;
+
+  &:hover {
+    opacity: 80%;
+  }
+`;
