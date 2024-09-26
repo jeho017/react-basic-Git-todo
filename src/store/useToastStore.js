67d50514caae2ebb3529a71a70aad1a93229aa8e
jeho017 <leejeho017@gmail.com>
2024-09-26 21:53:50 +0900

    feat: Zustand로 toast 스토어 추가하기

---
+++ src/store/useToastStore.js
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+
+export const useToastStore = create((set) => ({
+  toasts: [
+    {
+      id: crypto.randomUUID(),
+      content: "hello world",
+    },
+  ],
+  addToast: (content) => {
+    const newToast = {
+      id: crypto.randomUUID(),
+      content,
+    };
+
+    set((prevState) => ({
+      toasts: [...prevState.toasts, newToast],
+    }));
+  },
+  removeToast: (id) =>
+    set((prevState) => ({
+      toasts: prevState.toasts.filter((toastItem) => toastItem.id !== id),
+    })),
+}));
