2863e191aa0ec991b5eb69d252cff7c2441a80e0
jeho017 <leejeho017@gmail.com>
2024-10-08 23:19:09 +0900

    feat: 현재 투두 갯수 디스플레이 추가

--- components/todos/TodoController.tsx
+++ components/todos/TodoController.tsx
@@ -4,14 +4,20 @@ import React, { useId } from "react";
 import { Switch } from "../ui/switch";
 import { Label } from "../ui/label";
 import { useTodoStore } from "@/store/useTodoStore";
+import { useTodoQuery } from "@/query/useTodoQuery";
 
 const TodoController = () => {
   const completedSwitchId = useId();
   const { completed, toggleCompleted } = useTodoStore();
 
+  const { data: allTodos } = useTodoQuery();
+  const { data: completedTodos } = useTodoQuery("completed");
+
   return (
     <div className="flex flex-row justify-between px-4">
-      <p>0/4</p>
+      <p>
+        {completedTodos?.length}/{allTodos?.length}
+      </p>
       <div className="flex items-center space-x-2">
         <Switch
           id={completedSwitchId}
